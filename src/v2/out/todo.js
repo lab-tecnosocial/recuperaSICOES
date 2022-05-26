efd0ec5a58b21b4679d2103b2ddc52840151d484
Alex Ojeda Copa <alex-roc@users.noreply.github.com>
2022-05-25 21:50:08 -0400

    fix: campo entidad y objeto con caracteres especiales

--- src/v2/out/todo.js
+++ src/v2/out/todo.js
@@ -6,20 +6,32 @@ function capturaTabla() {
     for (i = 1; i <= 10; i++) {
         let fila = [];
         for (j = 1; j <= 20; j++) {
-            if (j >= 1 && j <= 4) {
+            if (j === 1) {
                 fila.push(document.querySelector(`#tablaAvanzada > tbody > tr:nth-child(${i}) > td:nth-child(${j})`).innerHTML);
-            } else if (j === 5) {
+            } 
+            else if (j === 2) {
+                let str = document.querySelector(`#tablaAvanzada > tbody > tr:nth-child(${i}) > td:nth-child(${j})`).innerHTML;
+                fila.push('"' + str + '"');
+            } 
+            
+            else if (j >= 3 && j <= 4) {
+                fila.push(document.querySelector(`#tablaAvanzada > tbody > tr:nth-child(${i}) > td:nth-child(${j})`).innerHTML);
+            }
+
+            else if (j === 5) {
                 let str = document.querySelector(`#tablaAvanzada > tbody > tr:nth-child(${i}) > td:nth-child(${j})`).innerHTML;
                 str = str.replace(/"/g, "'");
+                str = str.replace(/\n/g, " ");
                 fila.push('"' + str + '"');
-            } else if (j >= 6 && j <= 9) {
+            } 
+            else if (j >= 6 && j <= 9) {
                 fila.push(document.querySelector(`#tablaAvanzada > tbody > tr:nth-child(${i}) > td:nth-child(${j})`).innerHTML);
             }
             else if (j === 10) {
                 let stringArch = "";
                 let archivos = document.querySelector(`#tablaAvanzada > tbody > tr:nth-child(${i}) > td:nth-child(${j})`).querySelectorAll('a');
                 for (let i = 0; i < archivos.length; i++) {
-                    stringArch += i+ 1 + "." + archivos[i].innerText + " "
+                    stringArch += i + 1 + "." + archivos[i].innerText + " "
                 }
                 fila.push(stringArch);
             }
@@ -30,9 +42,12 @@ function capturaTabla() {
                     stringForm += i+ 1 + "." + forms[i].innerText + " "
                 }
                 fila.push(stringForm);
-            } else if (j === 12) {
+            } 
+            else if (j === 12) {
                 fila.push(document.querySelector(`#tablaAvanzada > tbody > tr:nth-child(${i}) > td:nth-child(${j})`).firstElementChild.href);
-            } else if (j >= 13 && j <= 20) {
+            
+            } 
+            else if (j >= 13 && j <= 20) {
                 fila.push(document.querySelector(`#tablaAvanzada > tbody > tr:nth-child(${i}) > td:nth-child(${j})`).innerHTML);
             }
         }
